5e946d2f81bfc930d896dc2255ac2d3c13daad23
Mathenriques <math.marqui@gmail.com>
2023-05-19 13:50:21 -0300

    Criação dos repositorios prisma da aplicação

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,7 +2,7 @@ import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
-  findById(checkInId: string): Promise<CheckIn | null>
+  findById(id: string): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   countByUserId(userId: string): Promise<number>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
